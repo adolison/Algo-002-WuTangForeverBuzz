f765bb4153fb5a985f493e9121c8f0ef3ba4c626
adolison <amdol1@morgan.edu>
2019-07-30 05:46:03 -0400

    WuTang 0-15 no extra lines

--- src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
+++ src/main/java/hbcu/stay/ready/algorithms/WuTangForeverBuzz.java
@@ -3,6 +3,51 @@ package hbcu.stay.ready.algorithms;
 public class WuTangForeverBuzz {
 
     public String wuTangClan(){
-        return null;
+        String response ="";
+        Integer x =0;
+
+        do{
+            x++;
+            if (x!=1) {
+                response += "\n";
+            }
+            if (x%3==0 && x%5==0){
+                //System.out.println("WuTang Forever");
+                response += "WuTang Forever";
+            }
+            else
+            if(x%3==0){
+                //System.out.println("Wu");
+                response += "Wu";
+            }else
+            if(x%5==0){
+                //System.out.println("Tang");
+                response += "Tang";
+            }else
+                //System.out.println(x);
+                response += x;
+
+
+        } while(x<15);
+        return response;
     }
 }
+/*for (int x=1;x<=15;x++){
+            if (x%3==0 && x%5==0){
+                //System.out.println("WuTang Forever");
+                response += "WuTang Forever";
+            }
+            else
+                if(x%3==0){
+                    //System.out.println("Wu");
+                    response += "Wu\n";
+                }else
+                    if(x%5==0){
+                        //System.out.println("Tang");
+                        response += "Tang\n";
+                    }else
+                        //System.out.println(x);
+                        response += x+"\n";
+
+
+        }*/
